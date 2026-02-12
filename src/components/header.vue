aa8a28795ee7ad0c39e0c7d8e5c455403f2dfb55
jd.chiu <jousindea@163.com>
2026-02-12 16:44:35 +0800

    feat: 新增版本号增强标识

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,11 +3,18 @@
     <template #content>
       <span class="title">{{ title }}</span>
     </template>
+    <template #extra>
+      <div class="version">
+        <span class="app-name">渐离App</span>
+        <span class="version-number">{{ pkg.version }}</span>
+      </div>
+    </template>
   </el-page-header>
 </template>
 
 <script lang="ts" setup>
 import { ArrowLeft } from '@element-plus/icons-vue'
+import pkg from '../../package.json'
 
 const props = defineProps({
   title: {
@@ -26,3 +33,16 @@ function back () {
   emit('back')
 }
 </script>
+
+<style lang="scss" scoped>
+.version {
+  display: flex;
+  flex-direction: row;
+  color: #5f5f5f;
+  gap: 6px;
+  .app-name {
+    font-size: 1.2em;
+    font-weight: 600;
+  }
+}
+</style>
